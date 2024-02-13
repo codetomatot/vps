eb24f618870c3076f9c112c27ac3695173e7ee5a
hueychopper <codelearn370@gmail.com>
2024-02-13 12:47:24 -0500

    finally fixed rotation...

--- robotics_coords/main.c
+++ robotics_coords/main.c
@@ -5,8 +5,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
-#include <limits.h>
 #include "matrix.h"
+
 
 #define PI 3.141592653589793
 #define WINDOW_HEIGHT 500
@@ -24,6 +24,9 @@ struct bottom_left convert_coords(float x, float y) {
 }
 
 char optlookup[2] = "ri";
+int g[3] =      {0x00,0xff,0x00};
+int p[3] =      {0xff,0xc0,0xcb};
+int orange[3] = {0xff,0xa5,0x00};
 
 void render_color(SDL_Renderer* r, int* c) {
     SDL_SetRenderDrawColor(r, c[0], c[1], c[2], 255);
@@ -76,27 +79,48 @@ void generate_point(SDL_Renderer* r, int32_t x_c, int32_t y_c, int32_t radius) {
     //i know it looks ugly but truly this algorithm works in about 500ms
 }
 
-double generate_arrow(SDL_Renderer* r, float x1, float y1, float x2, float y2, bool crt, int* color) {
+double* generate_arrow(SDL_Renderer* r, double* buffer,float x1, float y1, float x2, float y2, bool crt, int* color) {
     float y_t = convert_coords(x2,y2).yn;
     int c = 20; //arbitrarily set magnitude of a line
     double phi = acos(x2 / (sqrt((pow(x2,2.0) + pow(y2,2.0)))) );
     float left = PI + (phi/2.0);
     float right = PI + (3.0*phi/2.0);
     
-    // printf("phi is %f \n", phi);
 
-    // SDL_SetRenderDrawColor(r, 0,255,0,255);
     render_color(r, color);
     SDL_RenderDrawLine(r,x1,convert_coords(x1,y1).yn,x2,y_t);
-    // SDL_SetRenderDrawColor(r, 0,0,255,255);
+
     if(crt) {
         SDL_SetRenderDrawColor(r, 255,0,0,255);
         // render_color(r, color);
         SDL_RenderDrawLine(r,x2, y_t, (x2 + (c*cos(left))), (y_t - (c*sin(left))));
         SDL_RenderDrawLine(r,x2,y_t, x2 + (c*cos(right)), y_t - (c*sin(right)));
     }
+    
+    if(buffer != NULL) {
+        buffer[0] = x1;
+        buffer[1] = y1;
+        buffer[2] = x2;
+        buffer[3] = y2;
+    }
 
-    return phi;
+    return buffer;
+}
+void nonorigin_rotate(SDL_Renderer* render, float angle, float x1, float y1, float curr_x, float curr_y) {
+    double transform[4] = {0};
+    double* init = generate_arrow(render, transform, x1, y1, curr_x, curr_y, true, g);
+    //translate to origin
+    transform[2] -= transform[0];
+    transform[3] -= transform[1];
+    //rotate
+    float new_x = transform[2]*cos(angle) - transform[3]*sin(angle);
+    float new_y = transform[2]*sin(angle) + transform[3]*cos(angle);
+
+    //translate back to 50,50 transform[0],transform[1]
+    transform[2] = new_x + transform[0];
+    transform[3] = new_y + transform[1];
+
+    generate_arrow(render, NULL, x1, y1, transform[2], transform[3], false, g);
 }
 
 void clear_scheme(SDL_Renderer* render) {
@@ -121,7 +145,7 @@ int main(int argc, char* argv[]) {
         printf("eror init sdl");
         exit(1);
     }
-    SDL_Window* win = SDL_CreateWindow("Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOWN_WIDTH, WINDOW_HEIGHT, 0); //turns out top left corner is origin (0,0)
+    SDL_Window* win = SDL_CreateWindow("Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOWN_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL); //turns out top left corner is origin (0,0)
     SDL_Renderer* render = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
     SDL_SetRenderDrawColor(render,0,0,0,255);
     SDL_RenderClear(render);
@@ -145,14 +169,15 @@ int main(int argc, char* argv[]) {
     */
     SDL_Rect rect = {.x=200,.y=200,.h=40,.w=40};
 
-    int g[3] =      {0x00,0xff,0x00};
-    int p[3] =      {0xff,0xc0,0xcb};
-    int orange[3] = {0xff,0xa5,0x00};
 
     float curr_y = WINDOW_HEIGHT-(rect.y+(rect.h/2)); //center of square with respect to origin bl
     float curr_x = rect.x+(rect.w/2); //center of square 
-    const float magnitude_of_yaxis = sqrt(pow(curr_x,2)+pow(curr_y,2));
-    const float magnitude_of_xaxis = sqrt(pow(curr_x,2)+pow(curr_y,2));
+    //init
+    // nonorigin_rotate(render, (PI), 50,50,curr_x,curr_y);
+
+    //nonorigin_rotate(render, 0, curr_x,curr_y,curr_x,curr_y+60);
+    //nonorigin_rotate(render, (PI/6), curr_x,curr_y,curr_x,curr_y+60); //render, angle, originx, originy, lineend1,lineend2
+
 
     SDL_RenderPresent(render);
 
@@ -171,25 +196,14 @@ int main(int argc, char* argv[]) {
                 break;
             }
         }
-        // create_border(render, 0, 0, 10, 800);
-        float new_x = (curr_x)*cos(-((angle*PI)/180)) - (curr_y)*sin(-((angle*PI)/180));
-        float new_y = (curr_x)*sin(-((angle*PI)/180)) + (curr_y)*cos(-((angle*PI)/180));
+        SDL_SetRenderDrawColor(render,0x00,0x00,0x00,0xff);
+        SDL_RenderClear(render);
+        SDL_RenderCopyEx(render, txt, NULL, &rect, -angle, NULL, SDL_FLIP_NONE);
+        nonorigin_rotate(render, (PI*angle)/180, curr_x,curr_y, curr_x,curr_y+60);
+        SDL_RenderPresent(render);
+        angle+=30;
+        SDL_Delay(1000/30);
 
-        SDL_SetRenderDrawColor(render, 0x00, 0x00, 0x00, 0x00);
-    SDL_RenderClear(render);
-    SDL_RenderCopyEx(render,txt,NULL,&rect,angle,NULL,SDL_FLIP_NONE);
-    // SDL_RenderCopy(render, txt, NULL, &rect);
-    generate_arrow(render, 0,0,rect.x+(rect.w/2),WINDOW_HEIGHT-(rect.y+(rect.h/2)), true, g);
-    generate_arrow(render, rect.x+(rect.w/2), WINDOW_HEIGHT-(rect.y+(rect.h/2)), curr_x,curr_y, false, p); //y-axis
-    // generate_arrow(render, curr_x, curr_y, rect.x+60, curr_y, false, orange); //x-axis
-        SDL_RenderPresent(render); 
-        angle++;
-
-        // printf("current magnitude_y: %.2f. current mag_x: %.2f\n", magnitude_of_yaxis, magnitude_of_xaxis); //should be constants
-        printf("current new_x: %.2f. current new_y: %.2f\n", new_x, new_y);
-        
-        SDL_Delay(10000/10);
-        // SDL_Quit();
     }
     // free(m1); //uncomment when running matrices computation
     SDL_DestroyTexture(txt);
